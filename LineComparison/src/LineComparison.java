5269e2d4316d2e7a4d88698ab24d38780f770e93
Aman <aman.kumar99499@gmail.com>
2022-08-13 13:45:47 +0530

    Files

--- LineComparison/src/LineComparison.java
+++ LineComparison/src/LineComparison.java
@@ -1,5 +1,5 @@
+import java.util.Objects;
 import java.util.Scanner;
-
 public class LineComparison {
     //Constants
     static Double x1, x2, y1, y2, p1, p2, q1, q2;
@@ -8,8 +8,16 @@ public class LineComparison {
         System.out.println("Welcome to Line Comparison Program");
         LineComparison.EnterDistance();
         LineComparison.lineLength();
+        LineComparison.lineLengthTwo();
+        LineComparison.difference();
     }
-
+    public static void difference(){
+        if ((Objects.equals(lineLength(), lineLengthTwo()))) {
+            System.out.println("The Length are equal");
+        } else {
+            System.out.println("The length are not Equal");
+        }
+    }
     //Enter the distance values
     public static void EnterDistance() {
         System.out.println("Enter the Values of the following");
@@ -22,10 +30,23 @@ public class LineComparison {
         y1 = Double.valueOf(scan.next());
         System.out.println("y2");
         y2 = Double.valueOf(scan.next());
+        System.out.println("Enter the value for the Next Line");
+        System.out.println("p1");
+        p1 = Double.valueOf(scan.next());
+        System.out.println("p2");
+        p2 = Double.valueOf(scan.next());
+        System.out.println("q1");
+        q1 = Double.valueOf(scan.next());
+        System.out.println("q2");
+        q2 = Double.valueOf(scan.next());
     }
     //finding the length of the line
-    public static void lineLength() {
-        int length = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of the line is : " +length);
+    public static double lineLength() {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+    //finding the length 2nd time
+    public static Double lineLengthTwo() {
+        return (Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1)));
     }
 }
+
